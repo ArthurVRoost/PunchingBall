59014adec087733e087fd25f8fbdd89de6484997
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-13 11:11:00 +0200

    progress changement

--- src/components/progress/Progress.jsx
+++ src/components/progress/Progress.jsx
@@ -8,7 +8,7 @@ export default function Progress({vie}) {
         <div className='progress-bar-container'>
             {/* PERMET DE FAIRE DIMINUER LA BARRE DE PROGRESSION */}
             <div className='progress-bar-fill' style={{width: `${vie}%`}}>
-                <span className='progress-bar-text'>{vie}/100 PV</span>
+                {/* <span className='progress-bar-text'>{vie}/100 PV</span> */}
             </div>
         </div>
         </>
